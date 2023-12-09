d2dc92bf36bda406f93296715876e30d265ed201
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 03:41:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+// Question 01
+var age = "I am  18 Year Old"
+alert(age)
+
+// Question 02
+var visited ="You have visited this site 14 times"
+alert(visited)
+
+// Question 03
+var birthYear = "My birthYear Year is 1990"
+document.write(birthYear)
+document.write("Data type of my ddeclared variable is number")
+
+document.write("<br>"+"<br>"+"<br>")     
+// Question 04 
+var Visitorname = "SALMAN YASEEN  "
+
+var Producttitle = " T-Shirt (s)  "
+
+var Quantity = " 05"
+
+var order = Visitorname+Producttitle+Quantity
+
+document.write(order)
+// var ordername ="Ali"
+// var product_title ="T-shirt"
+// var quantity ="5"
+// document.write(ordername)
